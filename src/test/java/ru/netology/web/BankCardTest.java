4ad32e3ae5c8964d02fb1489efcaf5d3ea9d172b
Valeria V <volkovich-va@mail.ru>
2023-12-18 17:05:37 +0300

    Added changed

--- src/test/java/ru/netology/web/BankCardTest.java
+++ src/test/java/ru/netology/web/BankCardTest.java
@@ -17,8 +17,7 @@ public class BankCardTest {
     private WebDriver driver;
 
     @BeforeAll
-    public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
+    public static void setupAll() { WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
@@ -48,45 +47,8 @@ public class BankCardTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
         assertTrue(actualTextElement.isDisplayed());
     }
-
     @Test
-    public void shouldTestV2() /* Фамилия + имя (без пробела) */ {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("ИвановИван");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79273334242");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button")).click();
-        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
-        var actualText = actualTextElement.getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
-        assertTrue(actualTextElement.isDisplayed());
-    }
-
-    @Test
-    public void shouldTestV3() /* Фамилия + 5 пробелов + имя */ {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов     Иван");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79273334242");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button")).click();
-        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
-        var actualText = actualTextElement.getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
-        assertTrue(actualTextElement.isDisplayed());
-    }
-
-    @Test
-   public void shouldTestV4() /* Дефис + фамилия + дефис + имя + дефис */ {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("-Иванов-Иван-");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79273334242");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button")).click();
-        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
-        var actualText = actualTextElement.getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
-        assertTrue(actualTextElement.isDisplayed());
-    }
-
-    @Test
-    public void shouldTestV5() /* Ошибка валидации в поле "Фамилия и имя". Ввод латиницы. */ {
+    public void shouldTestV2() /* Ошибка валидации в поле "Фамилия и имя". Ввод латиницы. */ {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivanov Ivan");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79273334242");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -98,7 +60,7 @@ public class BankCardTest {
    }
 
     @Test
-    public void shouldTestV6() /* Ошибка валидации в поле "Фамилия и имя". Ввод кириллицы с числом. */ {
+    public void shouldTestV3() /* Ошибка валидации в поле "Фамилия и имя". Ввод кириллицы с числом. */ {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван5");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79273334242");
         driver.findElement(By.className("checkbox__box")).click();
@@ -110,7 +72,7 @@ public class BankCardTest {
     }
 
     @Test
-    public void shouldTestV7() /* Ошибка валидации в поле "Фамилия и имя". Ввод кириллицы с символами. */ {
+    public void shouldTestV4() /* Ошибка валидации в поле "Фамилия и имя". Ввод кириллицы с символами. */ {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов? Иван!");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79273334242");
         driver.findElement(By.className("checkbox__box")).click();
@@ -122,7 +84,7 @@ public class BankCardTest {
     }
 
     @Test
-    public void shouldTestV8() /* Пустое поле ввода имени и фамилии */ {
+    public void shouldTestV5() /* Пустое поле ввода имени и фамилии */ {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79273334242");
         driver.findElement(By.className("checkbox__box")).click();
@@ -134,7 +96,7 @@ public class BankCardTest {
     }
 
     @Test
-   public void shouldTestV9() /* Телефон из нулей */ {
+   public void shouldTestV6() /* Телефон из нулей */ {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+00000000000");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -146,7 +108,7 @@ public class BankCardTest {
     }
 
     @Test
-    public void shouldTestV10() /* Ввод номера без "+". */ {
+    public void shouldTestV7() /* Ввод номера без "+". */ {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("79273334242");
         driver.findElement(By.className("checkbox__box")).click();
@@ -158,7 +120,7 @@ public class BankCardTest {
     }
 
     @Test
-    public void shouldTestV11() /* Пустое поле ввода телефона. */ {
+    public void shouldTestV8() /* Пустое поле ввода телефона. */ {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
         driver.findElement(By.className("checkbox__box")).click();
@@ -170,7 +132,7 @@ public class BankCardTest {
     }
 
     @Test
-    public void shouldTestV12() /* Отправление формы без галочки в чек-боксе */ {
+    public void shouldTestV9() /* Отправление формы без галочки в чек-боксе */ {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79273334242");
         driver.findElement(By.className("button")).click();
